e77a589da9b7cb8059138fec9c34d0662333dfba
braco96 <bravocolladoluis@gmail.com>
2023-06-12 13:14:00 +0000

    commit 01/08

---
+++ src/main/java/com/braco96/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
@@ -0,0 +1,9 @@
+package com.braco96.algoritmosestructuras.sorting._00_sorting;
+
+public class SearchUtils {
+  static void swap(int[] array, int i, int j) {
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  }
+}
